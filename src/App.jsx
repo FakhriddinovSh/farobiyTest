cebfd44d2814fdda37b98d08f1854ee3a6d1c91a
FakhriddinovSh <shaxoiddinfaxriddinov4@gmail.com>
2023-05-31 11:42:44 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import './app.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+
+export const App = () => {
+	return <div>App</div>;
+};
